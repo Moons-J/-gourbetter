3155a3c7148f5c9c5d432043698c4d744cd36d32
Carl Busch <carl@123busch.de>
2023-11-21 12:42:02 +0100

    added the first part of the index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,10 @@
 class RecipesController < ApplicationController
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @recipes = Recipe.where("title LIKE ?", "%#{params[:query]}%")
+    else
+      @recipes = Recipe.all
+    end
+  end
 end
