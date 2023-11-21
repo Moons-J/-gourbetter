7bbcce398b5f001ddb141eb8e9382c5c74ad6489
jonasmoons <moons_jonas@outlook.com>
2023-11-21 11:05:22 +0100

    fixed basic skeleton

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :recipes, through: :purchases
+  has_many :purchases
+  has_many :purchased_recipes, through: :purchases, source: :recipe
   has_many :recipes
   has_many :ratings
 end
