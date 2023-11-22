c636edb6b8e69c26d71ccedcca04c216bfd9fffb
Valeriia Chavdar <valera.chavdar@mail.ru>
2023-11-22 15:36:40 +0100

    Add purchase route

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,5 +2,9 @@ class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def new
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  def create
   end
 end
