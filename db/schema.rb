6fff531edaaba5ab56883326263d994f5be3812d
Valeriia Chavdar <valera.chavdar@mail.ru>
2023-11-22 16:54:29 +0100

    Add raiting average

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_20_124904) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_22_155318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,9 +60,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_20_124904) do
     t.bigint "user_id"
     t.string "category"
     t.integer "number_of_people"
-    t.float "average_rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "average_rating"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
