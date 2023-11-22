64c4fd2fda544421fe0e0942b6b8f8989e030b68
Valeriia Chavdar <valera.chavdar@mail.ru>
2023-11-22 16:27:27 +0100

    create action for purchases controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,10 +1,21 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: %i[new create]
 
   def new
     @recipe = Recipe.find(params[:recipe_id])
   end
 
   def create
+    recipe = Recipe.find(params[:recipe_id])
+
+    purchase = Purchase.new()
+    purchase.user = current_user
+    purchase.recipe = recipe
+    purchase.total_price = recipe.price
+    if purchase.save
+      redirect_to root_path
+    else
+      render :new, status: 422
+    end
   end
 end
